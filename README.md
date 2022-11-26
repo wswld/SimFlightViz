7ee995f5589f91b72dbeda225dce35f479656866
Vsevolod Glumov <seva17@gmail.com>
2022-11-26 13:02:41 -0800

    Init

---
+++ README.md
@@ -0,0 +1,8 @@
+# SimFlightViz
+
+Sometimes we spend too much time in MSFS 2020 and want to brag about that to 
+our friends with a beautiful visualisation like this:
+
+![](example.png)
+
+Still work in progress. Feel free to contribute or clone.
---
+++ simflightviz/simflightviz.py
@@ -0,0 +1,89 @@
+from mpl_toolkits.basemap import Basemap
+import numpy as np
+import matplotlib.pyplot as plt
+from dataclasses import dataclass
+import csv
+import os
+from datetime import datetime, timedelta
+from typing import Any
+from statistics import mean
+
+
+@dataclass
+class Stop:
+    icao: str
+    lat: float
+    lon: float
+    arrival: datetime
+    departure: datetime
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        if name in ['lat', 'lon']:
+            value = float(value)
+        super(Stop, self).__setattr__(name, value)
+
+
+def _initialize_map(m: Basemap) -> None:
+    m.fillcontinents(color='#ffd78c',lake_color='#68BBE3')
+    m.drawrivers(color='#68BBE3')
+    m.drawmapboundary(fill_color='#68BBE3')
+    m.drawcountries(linestyle='dotted')
+
+
+fig = plt.figure()
+
+with open(os.path.dirname(__file__)+'/flightlog.csv') as source:
+    stops = [Stop(**x) for x in list(csv.DictReader(source))]
+
+    min_lon = min([x.lon for x in stops])
+    min_lat = min([x.lat for x in stops])
+    max_lon = max([x.lon for x in stops])
+    max_lat = max([x.lat for x in stops])
+    
+    m = Basemap(
+        llcrnrlon=min_lon-5.,
+        llcrnrlat=min_lat-3.,
+        urcrnrlon=max_lon+5.,
+        urcrnrlat=max_lat+3.,
+        rsphere=(6378137.00,6356752.3142),
+        resolution='h',
+        projection='merc'
+    )
+    _initialize_map(m)
+
+    for i,stop in enumerate(stops):
+        m.scatter(
+            x=[stop.lon],
+            y=[stop.lat], 
+            latlon=True, 
+            marker="x",
+            color='#ff24ff', 
+            label=[stop.icao]
+        )
+        plt.text(
+            *m(stop.lon+0.1, stop.lat+0.1), 
+            stop.icao, 
+            zorder=99.9, 
+            fontsize="x-small",
+            fontweight="bold"
+        )
+        if i == 0:
+            continue
+        last_stop = stops[i-1]
+        m.plot(
+            x=[last_stop.lon,stop.lon],
+            y=[last_stop.lat,stop.lat], 
+            color='#ff24ff', 
+            latlon=True
+        )
+        time_en_route = (datetime.fromisoformat(stop.arrival)
+                         -datetime.fromisoformat(last_stop.departure))
+        plt.text(*m(
+            mean([last_stop.lon, stop.lon])+0.1, 
+            mean([last_stop.lat, stop.lat])+0.1), 
+            '{:02}:{:02}'.format(time_en_route.seconds//3600, (time_en_route.seconds//60)%60),
+            backgroundcolor="#ff24ff",
+            color="#ffffff",
+            fontsize="xx-small")
+
+plt.show()
